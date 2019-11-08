e64e3b107715a3f221f4e30c83d8385a10ed9fc4
Kingma <e_kingma@ats-hr420-dm19.fc.ca>
2019-11-08 13:47:08 -0500

    week 9 adding

---
+++ gameFunctions/gameVars.py
@@ -0,0 +1,11 @@
+from random import randint
+
+weapons=["rock", "paper", "scissors"]
+
+player_lives = 1
+computer_lives = 1
+
+total_lives = 1
+#start doing some logic and condition checking
+computer=weapons[randint(0,2)]
+player = False
