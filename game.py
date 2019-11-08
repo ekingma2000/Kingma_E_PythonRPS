8b346da6d7382e5b38830b04c7fb97626f2abb7e
Kingma <e_kingma@ats-hr420-dm19.fc.ca>
2019-11-08 14:02:28 -0500

    moving code to a different folder

--- game.py
+++ game.py
@@ -19,47 +19,12 @@ while gameVars.player is False:
 	#
 	# start doing some logic and condition checking 
 	# always chack a breaking condition first
-	if player == gameVars.computer:
-		# we have a tie, mo point in going any further
-		print("tie, no one wins! try again")
-
-	elif player == "quit":
-		print("you choose to quit, quitter.")
-		exit()
-
-	elif player == "rock":
-		if gameVars.computer == "paper":
-			print("You lose!", gameVars.computer, "covers", player, "\n")
-			gameVars.player_lives = gameVars.player_lives -1
-		else:
-			print("You win!", player, "smashes", gameVars.computer, "\n")
-			gameVars.computer_lives = gameVars.computer_lives -1
-
-	elif player == "paper":
-		if gameVars.computer == "scissors":
-			print("You lose!", gameVars.computer, "cuts", player, "\n")
-			gameVars.player_lives = gameVars.player_lives -1
-
-		else:
-			print("You win!", player, "covers", gameVars.computer, "\n")
-			gameVars.computer_lives = gameVars.computer_lives -1
-		
-	elif player == "scissors":
-		if gameVars.computer == "rock":
-			print("You lose!", gameVars.computer, "smashes", player, "\n")
-			gameVars.player_lives = gameVars.player_lives -1
-
-		else:
-			print("You win!", player, "cuts", gameVars.computer, "\n")
-			gameVars.computer_lives = gameVars.computer_lives -1
-
 	if gameVars.player_lives is 0:
 		winlose.winorlose("lost")
 
 	elif gameVars.computer_lives is 0:
 		winlose.winorlose("won")
-		
-		
 
-	player =False
-	gameVars.computer = gameVars.weapons[randint(0,2)]
+	else:
+		player =False
+		gameVars.computer = gameVars.weapons[randint(0,2)]
