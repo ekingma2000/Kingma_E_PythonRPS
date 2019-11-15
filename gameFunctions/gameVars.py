a924215c2b5806067756bc3bb6bebde8dd1f6905
Kingma <e_kingma@ats-hr420-dm21.fc.ca>
2019-11-15 16:06:40 -0500

    stylizing the code

--- gameFunctions/gameVars.py
+++ gameFunctions/gameVars.py
@@ -2,10 +2,10 @@ from random import randint
 
 weapons=["rock", "paper", "scissors"]
 
-player_lives = 1
-computer_lives = 1
+player_lives = 2
+computer_lives = 2
 
-total_lives = 1
+total_lives = 2
 #start doing some logic and condition checking
 computer=weapons[randint(0,2)]
 player = False
--- gameFunctions/winlose.py
+++ gameFunctions/winlose.py
@@ -1,22 +1,32 @@
+# import the random package so we can generate a random AI choice
 from random import randint
+# Import the gameVars file
 from gameFunctions import gameVars
 
 def winorlose(status):
-	print("called win or lose", status)
-	print("you", status, "! Would you like to play again")
+	print("\n\n\n")
+	print("               * * * * * * * * * * * * * * * * * * * * * * * * * *")
+	print("               *    You", status, "! Would you like to play again?     *")
+	print("               * * * * * * * * * * * * * * * * * * * * * * * * * *\n\n\n\n\n\n\n\n\n\n\n")
 	choice = input ("Y / N? ")
 
 	if choice == "Y" or choice == "y":
  			#reset the game and start all over again
-		gameVars.player_lives = 1
-		gameVars.computer_lives = 1
+		gameVars.player_lives = 2
+		gameVars.computer_lives = 2
 		gameVars.player = False 
 		gameVars.computer = gameVars.weapons[randint(0,2)]
 
 	elif choice == "N" or choice == "n":
-			print("You chose to quit")
-			exit()
+		print("\n\n\n\n\n\n\n")
+		print("               * * * * * * * * * * * * * * * * * * * * * * * * *")
+		print("               *      You Chose to Quit. Please Play Again!    *")
+		print("               * * * * * * * * * * * * * * * * * * * * * * * * * \n\n\n\n\n\n\n\n\n\n\n")
+		exit()
 	else: 
-			print("Make a vaild Choice. Yes or No!!")
+		print("\n\n\n\n\n\n\n")
+		print("               * * * * * * * * * * * * * * * * * * * * * * * * *")
+		print("               *          Please input a Valid Choice!         *")
+		print("               * * * * * * * * * * * * * * * * * * * * * * * * * \n\n\n\n\n\n\n\n\n\n\n")
 			# recursion => calling a funtion from inside itself
-			winorlose(status)
+		winorlose(status)
